3304821c398ce36782240264389ef5c63009450f
alscotty <alawrencescott@gmail.com>
2020-02-10 16:10:43 -0800

    binary search

---
+++ binary_search_project/lib/binary_search.js
@@ -0,0 +1,43 @@
+function binarySearch(array, target) {
+    
+    let mid_idx=Math.floor(array.length/2);
+    if(array[mid_idx]===target) return true;
+    if(array.length<1) return false;
+
+    let left=array.slice(0,mid_idx);
+    let right=array.slice(mid_idx+1);
+
+    if(target>array[mid_idx]){
+        return binarySearch(right,target);
+    } else {
+        return binarySearch(left,target);
+    }
+    
+}
+
+function binarySearchIndex(array, target) {
+
+    let mid_idx = Math.floor(array.length / 2);
+    if (array[mid_idx] === target) return mid_idx;
+    if (array.length <= 1) return -1;
+    
+    let left = array.slice(0, mid_idx);
+    let right = array.slice(mid_idx + 1);
+    
+    if (target > array[mid_idx]) {
+        let res = binarySearchIndex(right, target);
+        console.log(res)
+        if(res==-1) return -1;
+        if(res !== undefined) return res+mid_idx+1
+    } else {
+        return binarySearchIndex(left, target);
+    }
+    
+
+}
+
+
+module.exports = {
+    binarySearch,
+    binarySearchIndex
+};
