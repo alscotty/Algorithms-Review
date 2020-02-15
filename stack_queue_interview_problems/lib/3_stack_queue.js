49bc1ba0197940fcdc29a4be236d750b45dca818
alscotty <alawrencescott@gmail.com>
2020-02-14 16:11:55 -0800

    stack & queue

---
+++ stack_queue_interview_problems/lib/3_stack_queue.js
@@ -0,0 +1,52 @@
+// ============================================================================
+// Interview Problem: StackQueue
+// ============================================================================
+//
+// -------
+// Prompt:
+// -------
+//
+// Implement your preferred Stack implementation, including the methods:
+//
+//   - Push 
+//   - Pop 
+//   - Size
+//
+// Then, implement a Queue by instantiating two Stack instances for storage.
+//
+// The StackQueue implementation should include the following methods:
+//
+//   - Enqueue
+//   - Dequeue
+//   - Size
+//
+// -----------
+// Let's code!
+// -----------
+
+class Node {
+    // TODO: Implement the Node class!
+    constructor(val){
+        this.value=val;
+        this.next=null;
+    }
+}
+
+class Stack {
+    // TODO: Implement the Stack class!
+    constructor(){
+        this.top=null;
+        this.bottom=null;
+        this.length=0;
+    }
+
+}
+
+class StackQueue {
+    // TODO: Implement the StackQueue class!
+
+};
+
+exports.Node = Node;
+exports.Stack = Stack;
+exports.StackQueue = StackQueue;
