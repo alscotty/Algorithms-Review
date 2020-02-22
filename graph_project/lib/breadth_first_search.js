d4ec49d444b330f0d0821a6cbc6e1465f912ea79
alscotty <alawrencescott@gmail.com>
2020-02-21 16:45:18 -0800

    graphs

---
+++ graph_project/lib/breadth_first_search.js
@@ -0,0 +1,20 @@
+function breadthFirstSearch(startingNode, targetVal) {
+    let visited = new Set()
+
+    let queue=[startingNode]
+
+    while (queue.length){
+        let node=queue.shift();
+        if (visited.has(node)) continue;
+        visited.add(node);
+        if (node.val===targetVal) return node;
+
+       queue.push(...node.neighbors)
+    }
+
+    return null
+}
+
+module.exports = {
+    breadthFirstSearch
+};
