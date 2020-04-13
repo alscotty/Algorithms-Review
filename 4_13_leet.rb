5ebde5aa33225d89f4b59b727eeb54042a1a5260
alscotty <alawrencescott@gmail.com>
2020-04-13 13:27:49 -0700

    hcakerrank

---
+++ 4_13_leet.rb
@@ -0,0 +1,9 @@
+#SQL Query:
+
+# SELECT EMPLOYEE_UIN.UIN, EMPLOYEE.NAME
+# FROM EMPLOYEE
+#     JOIN EMPLOYEE_UIN
+#     ON employee.id = employee_uin.id
+# WHERE Employee.age <25
+# ORDER BY Employee.name, employee.id ASC
+
