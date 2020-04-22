544fe2fa320c3a6e100a52f5a3caa7270e8dafe0
alscotty <alawrencescott@gmail.com>
2020-04-22 15:13:08 -0700

    max heap

--- 4_22.rb
+++ 4_22.rb
@@ -42,3 +42,82 @@ def remove_elements(head, val)
     head
 end
 
+# @param {Integer[]} nums
+# @param {Integer} k
+# @return {Integer}
+
+
+def find_kth_largest(nums, k)
+    nums.sort!
+    nums[-k]
+end
+
+# @param {Integer[]} nums
+# @param {Integer} k
+# @return {Integer}
+
+class MaxHeap 
+    attr_accessor :array
+    
+     def initialize()
+        @array = [null]
+     end
+    
+    def get_parent(idx)
+        (idx/2)
+    end
+    
+    def get_left_child(idx)
+        idx*2 
+    end
+    
+    def get_right_child(idx)
+        (idx*2) + 1
+    end
+    
+    def sift_up(idx)
+        return if idx <=1
+        
+        parent_idx = self.get_parent(idx)
+        current = self.array[idx]
+        parent = self.array[parent_idx]
+        
+        if current > parent
+            self.array[parent_idx], self.array[idx] = self.array[idx], self.array[parent_idx]
+            
+            
+            self.sift_up(parent_idx)
+        end
+    end
+
+    def insert(val)
+        self.array.push(val)
+        self.sift_up(self.array.length - 1)
+    end
+    
+    def delete_max()
+
+    end
+
+    def sift_down(idx)
+
+    end
+    
+end
+
+#becomes O(n) I think, instead of using .sort which is n*log(n)
+
+
+def find_kth_largest(nums, k)
+    max_heap = MaxHeap.new()
+    
+    nums.each {|val| max_heap.insert(val)}
+
+    i=1
+    while i<k
+        heap.delete_max()
+        i+=1
+    end
+    
+    heap.delete_max()
+end
