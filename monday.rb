bcb35eaf3f205ad59e36158abed34f751caf2ac9
alscotty <alawrencescott@gmail.com>
2020-10-26 17:47:07 -0700

    more code

--- monday.rb
+++ monday.rb
@@ -66,3 +66,135 @@ class TimeMap
         ""
     end
 end
+
+# @param {String} s
+# @return {Boolean}
+
+def is_valid(s)
+    visited_left_brackets = []
+    left_brackets = "({["
+    
+    s.each_char do |char|
+        if left_brackets.include?(char)
+           visited_left_brackets << char 
+        elsif char == ')'
+            if visited_left_brackets[-1] == '('
+               visited_left_brackets.pop
+            else
+                return false
+            end
+        elsif char == '}'
+            if visited_left_brackets[-1] == '{'
+                visited_left_brackets.pop
+            else
+                return false
+            end
+        elsif char == ']'
+            if visited_left_brackets[-1] == '['
+                visited_left_brackets.pop
+            else
+                return false
+            end
+        end
+    end
+    
+    visited_left_brackets.length == 0
+end
+
+class MinStack
+    attr_reader :min_stack
+=begin
+    initialize your data structure here.
+=end
+    def initialize()
+        @min_stack = []
+        @current_min = nil
+    end
+
+
+=begin
+    :type x: Integer
+    :rtype: Void
+=end
+    def push(x)
+        @min_stack << x
+        if !@current_min || x < @current_min
+           @current_min = x 
+        end
+    end
+
+
+=begin
+    :rtype: Void
+=end
+    def pop()
+        last = @min_stack.pop
+        if @current_min == last
+           @current_min = @min_stack.min 
+        end
+    end
+
+
+=begin
+    :rtype: Integer
+=end
+    def top()
+        @min_stack.last
+    end
+
+
+=begin
+    :rtype: Integer
+=end
+    def get_min()
+        @current_min
+    end
+
+
+end
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack.new()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.get_min()
+
+# @param {String[]} strs
+# @return {String[][]}
+
+def are_anagrams?(str1,str2)
+   str1.split('').sort == str2.split('').sort 
+end
+
+def group_anagrams(strs)
+    grouped_anagrams = [[strs.shift]]
+    
+    strs.each do |word|
+       pushed = false
+       (0...grouped_anagrams.length).each do |anagram_idx|
+            if are_anagrams?(grouped_anagrams[anagram_idx][0],word)
+                grouped_anagrams[anagram_idx] << word                    
+                pushed = true
+            end
+       end
+       grouped_anagrams << [word] unless pushed
+    end
+        
+    grouped_anagrams
+end
+
+# Faster solution!
+# @param {String[]} strs
+# @return {String[][]}
+
+def group_anagrams(strs)
+    anagram_hash = Hash.new {|hash,key| hash[key] = []}
+    
+    strs.each do |word|
+        sorted_word = word.chars.sort
+        anagram_hash[sorted_word] << word
+    end
+    
+    anagram_hash.values
+end
