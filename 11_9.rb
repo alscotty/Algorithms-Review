210f0a533c6770845514898ec4b0ad3382e9faf1
alscotty <alawrencescott@gmail.com>
2020-11-09 15:42:40 -0800

    more

--- 11_9.rb
+++ 11_9.rb
@@ -17,3 +17,36 @@ def valid_mountain_array(arr)
     
     true
 end
+
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer[]}
+
+def two_sum(nums, target)
+    hash = Hash.new {|h,k| h[k] = []}
+    
+    nums.each_with_index do |num,i|
+       hash[num] << i
+    end
+        puts hash
+    hash.keys.each do |num|
+        complement = target - num
+        if num == complement && hash[num].length > 1
+            return hash[num]
+        elsif !hash[complement].empty? && num != complement
+           return [hash[num][0],hash[complement][0]] 
+        end
+    end
+    
+end
+
+# faster/simpler
+def two_sum(nums, target)
+  dict = {}
+  nums.each_with_index do |n, i|
+    if dict[target - n]
+      return dict[target - n], i
+    end
+    dict[n] = i
+  end
+end
