11150213e33c746e702acfead3df08b872704da6
alscotty <alawrencescott@gmail.com>
2021-01-02 17:12:39 -0800

    dynamic p practice

--- tabulation_project/lib/leet_code_64.js
+++ tabulation_project/lib/leet_code_64.js
@@ -2,5 +2,37 @@
 // Feel free to use this file for scratch work.
 
 function minPathSum(grid) {
+    let width = grid[0].length;
+    let height = grid.length;
 
+    let table = new Array(width).fill(new Array(height));
+    table[0][0] = grid[0][0];
+
+    for (let i = 0; i < table.length; i++) {
+
+    }
+
+    return table[width][height];
 }
+
+// var minPathSum = function (grid) {
+//     if (!grid) return 0;
+
+//     let row = grid.length;
+//     let column = grid[0].length;
+
+//     let res = [0];
+//     for (let i = 0; i < row; i++) {
+//         for (let j = 0; j < column; j++) {
+//             if (i === 0) {
+//                 res.push(res[j] + grid[i][j]);
+//             } else if (j === 0) {
+//                 res[j + 1] += grid[i][j];
+//             } else {
+//                 res[j + 1] = Math.min(res[j], res[j + 1]) + grid[i][j];
+//             }
+//         }
+//     }
+
+//     return res[column];
+// };
--- tabulation_project/lib/leet_code_70.js
+++ tabulation_project/lib/leet_code_70.js
@@ -1,6 +1,24 @@
 // Work through this problem on https://leetcode.com/problems/climbing-stairs/ and use the specs given there.
 // Feel free to use this file for scratch work.
 
-function climbStairs(n) {
+//tabulation:
 
+function climbStairs(n) {
+    if (n == 1) return 1;
+    let table = new Array(n + 1);
+    table[1] = 1;
+    table[2] = 2;
+    for (let i = 3; i <= n; i++) {
+        table[i] = table[i - 1] + table[i - 2];
+    }
+    return table[n];
+}
+
+//memoization
+function climbStairsMemo(n, memo = { 1: 1, 2: 2 }) {
+    if (memo[n]) return memo[n];
+
+    let nextAnswer = climbStairs(n - 1, memo) + climbStairs(n - 2, memo)
+    memo[n] = nextAnswer;
+    return nextAnswer;
 }
