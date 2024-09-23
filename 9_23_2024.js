ef0da09eea6388317d195d37b1d0de70c612c00a
alscotty <alawrencescott@gmail.com>
2024-09-23 14:27:31 -0700

    linked lists

--- 9_23_2024.js
+++ 9_23_2024.js
@@ -2,7 +2,7 @@
 
 const isAnagram = (word) => {
     let lastIdx = word.length - 1;
-    let midIdx = word.length/2;
+    let midIdx = word.length / 2;
 
     for (let idx = 0; idx < midIdx; idx++) {
         let leftChar = word[idx];
@@ -13,6 +13,42 @@ const isAnagram = (word) => {
     return true;
 }
 
-// Test cases:
-console.log(isAnagram('tacocat')) // true
-console.log(isAnagram('word')) // false
+// Test cases, passed:
+// console.log(isAnagram('tacocat')) // true
+// console.log(isAnagram('word')) // false
+
+
+// linkedList! reverse a linked list:
+class LinkedList {
+    constructor(val) {
+        this.val = val;
+        this.next = null
+    }
+}
+
+let head = new LinkedList(1)
+let firstNode = new LinkedList(2)
+head.next = firstNode;
+let secondNode = new LinkedList(3)
+firstNode.next = secondNode
+
+
+// given the head of a linkedlist, reverse it and return the new head!
+const reverseLinkedList = (head) => {
+    let prevNode = null
+    let currentNode = head;
+
+    while (currentNode) {
+        let savedNext = currentNode.next;
+        currentNode.next = prevNode;
+        
+        prevNode = currentNode;
+        currentNode = savedNext;
+    }
+
+    return prevNode;
+}
+
+// test passed!
+console.log(reverseLinkedList(head))
+console.log(secondNode)
