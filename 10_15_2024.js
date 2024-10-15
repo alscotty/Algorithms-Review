0d5644cfdef40677fd9896030d837455169b0d1a
alscotty <alawrencescott@gmail.com>
2024-10-15 16:00:48 -0700

    algorithms practice

--- 10_15_2024.js
+++ 10_15_2024.js
@@ -26,3 +26,57 @@ const isValid = function (s) {
 
     return stack.length === 0;
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ * 
+ * want the maximum depth of any given Binary tree
+ */
+var maxDepth = function (root, currentDepth = 0) {
+    if (!root) return currentDepth;
+
+    currentDepth++
+
+    return Math.max(maxDepth(root.left, currentDepth), maxDepth(root.right, currentDepth));
+};
+
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ * simple string/array manipulation:
+ * 
+ * Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+ 
+
+Example 1:
+
+Input: s = "abc", t = "ahbgdc"
+Output: true
+Example 2:
+
+Input: s = "axc", t = "ahbgdc"
+Output: false
+ */
+var isSubsequence = function (s, t) {
+    let subsequenceArray = s.split('');
+
+    for (let char of t) {
+        if (char == subsequenceArray[0]) subsequenceArray.shift();
+    }
+
+    return subsequenceArray.length == 0;
+};
